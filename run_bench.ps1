dcaf7379374d021fa50d807e646759aaf3049af5
Daru András <bihsrs@inf.elte.hu>
2026-02-18 12:40:11 +0100

    Changing commit messages

--- run_bench.ps1
+++ run_bench.ps1
@@ -3,5 +3,5 @@ Write-Host "Benchmarkok futtatása (ez eltarthat pár percig)..."
 cargo test --release -- --ignored --nocapture --test-threads=1
 
 git add benchmark_output.json
-git commit -m "Update Benchmark History [skip ci]"
+git commit -m "Update Benchmark History"
 git push
