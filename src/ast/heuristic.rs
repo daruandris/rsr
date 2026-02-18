bbdd24162eb0cba4eab3ab5f9494a98d150fe41a
Daru András <bihsrs@inf.elte.hu>
2026-02-18 10:28:47 +0100

    Simplify with replacing - - with +

--- src/ast/heuristic.rs
+++ src/ast/heuristic.rs
@@ -24,11 +24,13 @@ pub fn simplify_ast(original_nodes: &[Node]) -> Vec<Node> {
 
                 if arity == 1 {
                     if let Some(a) = stack.pop() {
+                        // Konstans egyszerűsítés
                         if a.is_const {
                             let res = match op {
                                 Op::Sin => a.val.sin(),
                                 Op::Cos => a.val.cos(),
                                 Op::Exp => a.val.exp(),
+                                Op::Sqr => a.val * a.val,
                                 _ => unreachable!(),
                             };
                             if res.is_finite() {
@@ -44,6 +46,7 @@ pub fn simplify_ast(original_nodes: &[Node]) -> Vec<Node> {
                 }
                 else if arity == 2 {
                     if let (Some(b), Some(a)) = (stack.pop(), stack.pop()) {
+                        // két konstans
                         if a.is_const && b.is_const {
                             let res = match op {
                                 Op::Add => a.val + b.val,
@@ -57,7 +60,7 @@ pub fn simplify_ast(original_nodes: &[Node]) -> Vec<Node> {
                                 continue;
                             }
                         }
-
+                        // azonos ágak
                         if a.nodes == b.nodes {
                             if let Op::Sub = op {
                                 stack.push(SubTree { nodes: vec![Node::Constant(0.0)], is_const: true, val: 0.0 });
@@ -69,10 +72,39 @@ pub fn simplify_ast(original_nodes: &[Node]) -> Vec<Node> {
                             }
                         }
                         
+                        // szorzás 0-val
                         if let Op::Mul = op {
                             if (a.is_const && a.val.abs() < 1e-9) || (b.is_const && b.val.abs() < 1e-9) {
                                 stack.push(SubTree { nodes: vec![Node::Constant(0.0)], is_const: true, val: 0.0 });
                                 continue;
+                            }
+                        }
+
+                        // A - (-B) = A + B
+                        if let Op::Sub = op {
+                            if b.nodes.len() >= 2 {
+                                if let Some(Node::Operator(Op::Sub)) = b.nodes.last() {
+                                    if let Some(Node::Constant(c)) = b.nodes.get(b.nodes.len() - 2) {
+                                        if c.abs() < 1e-9 {
+                                            let x_nodes = &b.nodes[0..b.nodes.len()-2];
+                                            if a.is_const && a.val.abs() < 1e-9 {
+                                                stack.push(SubTree { 
+                                                    nodes: x_nodes.to_vec(), 
+                                                    is_const: false,
+                                                    val: 0.0 
+                                                });
+                                                continue;
+                                            }
+                                            let mut new_nodes = Vec::with_capacity(a.nodes.len() + x_nodes.len() + 1);
+                                            new_nodes.extend(a.nodes);
+                                            new_nodes.extend_from_slice(x_nodes);
+                                            new_nodes.push(Node::Operator(Op::Add));
+                                            
+                                            stack.push(SubTree { nodes: new_nodes, is_const: false, val: 0.0 });
+                                            continue;
+                                        }
+                                    }
+                                }
                             }
                         }
 
